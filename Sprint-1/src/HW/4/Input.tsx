81ee953228b07ef2b04f4291b188a73663b55c39
Nikita Yerameey <smertskosoii@gmail.com>
2024-12-11 19:02:18 +0300

    Done homowork-4 sprint-1

--- Sprint-1/src/HW/4/Input.tsx
+++ Sprint-1/src/HW/4/Input.tsx
@@ -1,14 +1,19 @@
+import React, { ChangeEvent } from "react";
+
 type InputPropsType = {
-	currentText: any // НУЖНО ПРОТИПИЗИРОВАТЬ
-	setCurrentText: any // НУЖНО ПРОТИПИЗИРОВАТЬ
+	currentText: string // НУЖНО ПРОТИПИЗИРОВАТЬ
+	setCurrentText: (text: string) => void; // НУЖНО ПРОТИПИЗИРОВАТЬ
 };
 
 export const Input = (props: InputPropsType) => {
-	const onChangeHandler = (event: 'НУЖНО ПРОТИПИЗИРОВАТЬ') => {
+	const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
 		// НУЖНО ДОПИСАТЬ
+		const newText = event.target.value;
+        props.setCurrentText(newText);
+	
 	};
 
-	// return (
-	//   <input id={'hw04-input'} type="text" value={props.currentText} onChange={onChangeHandler} />
-	// );
+	 return (
+	   <input id={'hw04-input'} type="text" value={props.currentText} onChange={onChangeHandler} />
+	 );
 };
